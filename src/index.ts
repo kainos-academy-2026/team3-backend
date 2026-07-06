15129fe75206360697a63124325a15488ccc0477
DamianMcN <damianmcneice@hotmail.com>
2026-07-06 14:17:30 +0100

    PR changes made

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
 import app from "./app.js";
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
-  console.log(`Try: http://localhost:${PORT}/health`);
 });
