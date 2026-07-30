5136a6705b550aec85882a10c9b6c5c24e0b70d8
catkane05 <cathkane1998@gmail.com>
2026-07-30 12:18:59 +0100

    change ingress to internal only

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -90,7 +90,7 @@ resource "azurerm_container_app" "backend" {
   }
 
   ingress {
-    external_enabled = true
+    external_enabled = false
     target_port      = 4000
     traffic_weight {
       percentage      = 100
