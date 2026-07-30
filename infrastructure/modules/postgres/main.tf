ff927020942629f6bde11109b726437ad6fb0976
yasrig <yasmine.rigby@kainos.com>
2026-07-30 10:42:26 +0100

    correct postgres auto-grow arg and default allowed IP map in CI

--- infrastructure/modules/postgres/main.tf
+++ infrastructure/modules/postgres/main.tf
@@ -23,7 +23,7 @@ resource "azurerm_postgresql_flexible_server" "postgres" {
   sku_name = "B_Standard_B1ms"
 
   storage_mb                = 32768
-  storage_auto_grow_enabled = false
+  auto_grow_enabled         = false
 
   backup_retention_days        = 7
   geo_redundant_backup_enabled = false
