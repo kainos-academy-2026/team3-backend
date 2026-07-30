56a758371bc3db7cf23e27ba4c278ddc5bf3014a
yasrig <yasmine.rigby@kainos.com>
2026-07-30 10:47:44 +0100

    formatting error - just fixing

--- infrastructure/modules/postgres/main.tf
+++ infrastructure/modules/postgres/main.tf
@@ -22,8 +22,8 @@ resource "azurerm_postgresql_flexible_server" "postgres" {
 
   sku_name = "B_Standard_B1ms"
 
-  storage_mb                = 32768
-  auto_grow_enabled         = false
+  storage_mb        = 32768
+  auto_grow_enabled = false
 
   backup_retention_days        = 7
   geo_redundant_backup_enabled = false
